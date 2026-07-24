76e428ffc1970936aeb00f1ec68a4eb5d6b227d8
arminerika <arminerika@gmail.com>
2026-07-23 23:44:38 -0700

    clean up leftover vite boilerplate + add sona branding

--- sona-app/client/src/App.jsx
+++ sona-app/client/src/App.jsx
@@ -1,10 +1,13 @@
 import { Link, Outlet } from "react-router-dom";
+import logo from "./assets/sona-logo-tagline.svg";
 
 function App() {
   return (
     <>
       <nav className="nav">
-        <Link to="/" className="brand">Sona</Link>
+        <Link to="/" className="brand">
+          <img src={logo} alt="Sona" className="brand-logo" />
+        </Link>
         <div className="nav-right">
           <Link to="/profile">Profile</Link>
           <span className="cart">🛒</span>
