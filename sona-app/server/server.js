fdd3640bf78a5e67308bfe56c0009e0018e410e2
khanaw001 <wasaykhan2933@gmail.com>
2026-07-24 21:26:04 -0400

    added/completed order creation endpoint

--- sona-app/server/server.js
+++ sona-app/server/server.js
@@ -6,6 +6,7 @@ import usersRouter from "./routes/users.js";
 import followsRouter from "./routes/follows.js";
 import postsRouter from "./routes/posts.js";
 import merchRouter from "./routes/merch.js";
+import orderRoutes from "./routes/orders.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,6 +20,7 @@ app.use("/api/users", usersRouter);
 app.use("/api/follows", followsRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/merch", merchRouter);
+app.use('/api/orders', orderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Sona server running on http://localhost:${PORT}`);
